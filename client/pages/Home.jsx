e0ada78a726a9825046761d9d853c715e3fd8d04
mziyad10 <mvsiyad10@gmail.com>
2025-08-19 15:57:24 +0530

    feat: added testimonial section

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../src/components/Hero'
 import FeaturedSection from '../src/components/FeaturedSection'
+import Banner from '../src/components/Banner'
+import Testimonial from '../src/components/Testimonial'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <FeaturedSection/>
+      <Banner/>
+      <Testimonial/>
     </>
   )
 }
